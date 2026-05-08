0c12768998967e18dc72383999975ef53ba415ab
J0ce1ynV <jocelynkwok.pku@gmail.com>
2026-05-08 23:56:36 +0700

    Bullet added
    -Bullet behavior
    -Health pickup animation idle
    -Jump condition only when player is on the ground layer

--- Assets/Script/PlayerBehavior.cs
+++ Assets/Script/PlayerBehavior.cs
@@ -11,6 +11,15 @@ public class PlayerBehavior : MonoBehaviour
     private bool _isJumping;
     private Rigidbody _rb;
 
+    //lompat sekali k=hanya ketika nyentuh ground
+    public bool IsOnGround = true;
+    public float GroundCheckRadius = 1.05f;
+    public LayerMask GroundLayer;
+
+    public GameObject Bullet;
+    public float BulletSpeed = 100f;
+    private bool _isShooting;
+
     void Start()
     {
         _rb = GetComponent<Rigidbody>();
@@ -20,7 +29,17 @@ public class PlayerBehavior : MonoBehaviour
     {
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
-        _isJumping |= Input.GetKeyDown(KeyCode.Space);
+        _isJumping = Input.GetKeyDown(KeyCode.Space);
+
+        //lompat 
+        IsOnGround = Physics.CheckSphere(transform.position, GroundCheckRadius, GroundLayer);
+        if (Input.GetKeyDown(KeyCode.Space) && IsOnGround)
+        {
+            _isJumping = true;
+        }
+
+        //shooting
+        _isShooting |= Input.GetKeyDown(KeyCode.F);
     }
 
     void FixedUpdate()
@@ -38,5 +57,21 @@ public class PlayerBehavior : MonoBehaviour
             _rb.AddForce(Vector3.up * JumpVelocity, ForceMode.Impulse);
             _isJumping = false;
         }
+
+        //shooting
+        if (_isShooting)
+        {
+            Vector3 spawnPos = transform.position + transform.forward * 1f;
+            GameObject newBullet = Instantiate(Bullet, spawnPos, this.transform.rotation);
+            Rigidbody bulletRB = newBullet.GetComponent<Rigidbody>();
+            bulletRB.velocity = this.transform.forward * BulletSpeed;
+            _isShooting = false;
+        }
     }
+
+    //void OnDrawGizmosSelected()
+    //{
+    //    Gizmos.color = Color.green;
+    //    Gizmos.DrawWireSphere(transform.position, GroundCheckRadius);
+    //}
 }
